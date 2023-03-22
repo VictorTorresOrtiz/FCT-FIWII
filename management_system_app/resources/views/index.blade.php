911fce099044400587a4ff3cc739ebe91e61ac84
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-03-22 01:04:52 +0100

    PWA assets

--- management_system_app/resources/views/index.blade.php
+++ management_system_app/resources/views/index.blade.php
@@ -16,7 +16,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
 <!--PWA READY-->
-@lirewireStyles
 @laravelPWA
 
 </head>
