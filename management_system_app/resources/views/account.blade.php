52d4dc5304759e60cd4654c8c89a27f3722ac9b3
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-03-27 17:29:25 +0200

    Profile

--- management_system_app/resources/views/account.blade.php
+++ management_system_app/resources/views/account.blade.php
@@ -29,7 +29,7 @@
 
 <div id="page">
 
-    <!-- header and footer bar go here-->
+    <!-- header and footer bar-->
     <div class="header header-fixed header-auto-show header-logo-app">
         <a href="{{url('/')}}" data-back-button class="header-title header-subtitle">Volver</a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
