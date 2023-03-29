04db6af7b811d1e056be480a706230f198833868
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-03-29 20:35:58 +0200

    Detect Conexión

--- management_system_app/public/assets/js/custom.js
+++ management_system_app/public/assets/js/custom.js
@@ -1386,9 +1386,9 @@ document.addEventListener('DOMContentLoaded', () => {
             const offlineAlert = document.createElement('p');
             const onlineAlert = document.createElement('p');
             offlineAlert.className = 'offline-message bg-red-dark color-white';
-            offlineAlert.textContent = 'No internet connection detected';
+            offlineAlert.textContent = 'Conexión perdida con Fiwii Server';
             onlineAlert.className = 'online-message bg-green-dark color-white';
-            onlineAlert.textContent = 'You are back online';
+            onlineAlert.textContent = 'Conexión Recuperada';
             document.getElementsByTagName('body')[0].appendChild(offlineAlert);
             document.getElementsByTagName('body')[0].appendChild(onlineAlert);
         }
