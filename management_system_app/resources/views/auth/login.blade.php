8a7acf2b793d98635bffd2774c2839193b6fb6d8
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-04-25 00:44:32 +0200

    Front actualizado

--- management_system_app/resources/views/auth/login.blade.php
+++ management_system_app/resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <title>Testing</title>
     <link rel="stylesheet" href="{{ URL::asset('assets/css/auth.css') }}" />
 
 </head>
@@ -13,7 +13,7 @@
 	<div class="form-container sign-in-container">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-			<h1>Login</h1>
+			<h1>Acceder a Fiwii</h1>
 			<label for="email" class="col-md-4 col-form-label text-md-right"></label>
             <input id="email" type="email" placeholder="Email Address" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             @error('email')
@@ -34,15 +34,6 @@
                 {{ __('Login') }}
             </button>
 		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-right">
-				<h1>¿No tienes cuenta Fiwii?</h1>
-				<p>Ingresa tus datos personales y comienza tu viaje con nosotros</p>
-				<a class="nav-link" href="{{url ('register')}}"><button class="ghost" id="signUp">Crear cuenta</button></a>
-			</div>
-		</div>
 	</div>
 </div>
 </body>
