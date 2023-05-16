e7999fc1aa310c453102689675751ae92baaff01
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-05-16 22:19:21 +0200

    Crear Sistemas

--- fiwii/resources/views/admin/crearSistema.blade.php
+++ fiwii/resources/views/admin/crearSistema.blade.php
@@ -37,57 +37,49 @@
             </div>
         </nav>
     </header>
-    <main id="main" class="shock-main bg-color gray-10 p-4">
-        <h2 class="title black">
-            <span class="text-1 text-dark text-style-5">Añade sistemas a tu </span>
-            <span class="text-2 text-dark text-style-6 text-italic"><mark class="animated-underline accent">base de datos</mark>.</span>
-        </h2>
-        <div class="form-area scheme-1 secondary">
-            <form action="{{ route('addSystem') }}" method="POST" enctype="multipart/form-data" class="form-fields needs-validation ajax-form">
-                @csrf
-                <div class="form-row row">
-                    <div class="form-col form-floating col-12 col-md-6">
-                        <input type="text" name="" id="InputFloatingName" class="form-control" placeholder="Nombre" required="required">
-                        <label for="InputFloatingName" class="form-label">Nombre</label>
-                    </div>
-                </div>
-                <div class="form-row row">
-                    <div class="form-col form-floating col-12 col-md-6">
-                        <textarea type="text" class="form-control" id="inputPassword4" placeholder="Descripción" required></textarea>
-                        <label for="InputFloatingName" class="form-label">Descripción</label>
-                    </div>
-                </div>
-                <div class="form-row row">
-                    <div class="form-col form-floating col-12 col-md-6">
-                        <input type="text" name="" id="InputFloatingName" class="form-control" placeholder="Ubicación" required>
-                        <label for="InputFloatingName" class="form-label">Ubicación</label>
-                    </div>
-                </div>
-                <div class="form-row row">
-                    <div class="col-md-2">
-                        <label for="inputState" class="form-label">Estado</label>
-                        <select id="inputState" class="form-select">
-                            <option selected>Mantenimiento</option>
-                            <option>Revisado</option>
-                            <option>Averiado</option>
-                        </select>
-                    </div>
-                    <div class="col-md-2">
-                        <label for="inputState" class="form-label">Prioridad</label>
-                        <select id="inputState" class="form-select">
-                            <option selected>Alta</option>
-                            <option>Media</option>
-                            <option>Baja</option>
-                        </select>
-                    </div>
-                </div>
-                    <div class="col-12">
-                        <a href="{{ url('/admin.crearSistema') }}"><button type="submit" class="button mt-3 outline rounded accent accent-hover">
-                            <span class="button-text accent white-hover">Añadir</span>
-                        </button></a>
-                    </div>
-                </div>
-            </form>
+    <form action="{{ route('addSystem') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Nombre</label>
+            <input type="text" name="sistemaNombre" class="form-control" id="inputEmail4">
         </div>
-    </main>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Ubicación</label>
+            <input type="text" name="sistemaUbicacion" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">Descripcion</label>
+            <input type="text" class="form-control" name="sistemaDesc" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress2" class="form-label">Address 2</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="col-md-6">
+            <label for="inputCity" class="form-label">City</label>
+            <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="col-md-4">
+            <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <label for="inputZip" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="inputZip">
+        </div>
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+            </div>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </form>
 @endsection
