24b35deb74d695102f6874bd59cbccf92fc37cf9
Javier Antequera <javiantmine@gmail.com>
2023-05-23 10:23:11 +0200

    update

--- fiwii/app/Http/Controllers/DashbooardController.php
+++ fiwii/app/Http/Controllers/DashbooardController.php
@@ -30,7 +30,6 @@ class DashbooardController extends Controller
             $systemInsert->description = $request->sistemaDesc;
             $systemInsert->status = $request->sistemaStatus;
             $systemInsert->prioridad = $request->sistemaPrioridad;
-            $systemInsert->tipo = $request->sistemaTipo;
             $systemInsert->updated_at;
             $systemInsert->save();
             notify()->success('Sistema añadido correctamente!');
